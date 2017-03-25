e26d2b56be20fd0e182b74da65f9a02c5bb47066
hopo <hpsight@gmail.com>
2017-03-25 20:18:56 +0900

    tour golang basics ~END

---
+++ tourGolang/basics/type-conversions.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int = 4
+	var z string = string(x)
+	fmt.Println(z)
+}
---
+++ tourGolang/basics/type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := "abc" // change me!
+	fmt.Printf("v is of type %T\n", v)
+}
