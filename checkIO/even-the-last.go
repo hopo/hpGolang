9fe88ad39fe5982e2b60b19c1dff6281e8456bcf
hopo <hpsight@gmail.com>
2017-03-30 22:54:43 +0900

    cIO/ even-the-last

---
+++ checkIO/even-the-last.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func evenLast(nums []int) {
+	fmt.Println(nums)
+}
+
+func main() {
+    evenLast([]int{0, 1, 2, 3, 4, 5})	//30, "(0+2+4)*5=30"
+    evenLast([]int{1, 3, 5})	//30, "(1+5)*5=30"
+    evenLast([]int{6})	//36, "(6)*6=36"
+    //evenLast([]int)	//0, "An empty array = 0"
+}
+
+/*
+function evenLast(data) {
+    if(data.length){
+		var sum = 0;
+		for(var n = 0; n < data.length; n = n + 1){
+			if(n % 2 == 0){
+				sum = sum + data[n]
+			};
+		};
+		return sum * data[data.length-1]
+	}else{
+		return 0;
+	};
+};
+*/
