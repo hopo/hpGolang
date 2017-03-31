e813386b026a74f56c75db08ee58404ead22c1ca
hopo <hpsight@gmail.com>
2017-03-31 22:12:28 +0900

    -

--- CheckIO/non-unique-elements.go
+++ CheckIO/non-unique-elements.go
@@ -1,5 +1,21 @@
-"use strict";
+package main
 
+import (
+	"fmt"
+)
+
+func nonUniqueElements(data []int) {
+	fmt.Println(data)
+}
+
+func main() {
+    nonUniqueElements([]int{1, 2, 3, 1, 3})	//[1, 3, 1, 3], "1st example"
+    nonUniqueElements([]int{1, 2, 3, 4, 5})	//[], "2nd example"
+    nonUniqueElements([]int{5, 5, 5, 5, 5})	//[5, 5, 5, 5, 5], "3rd example"
+    nonUniqueElements([]int{10, 9, 10, 10, 9, 8})	//[10, 9, 10, 10, 9], "4th example"
+}
+
+/*
 function nonUniqueElements(data) {
 	for(var i=0; i<data.length; i++) {
 		var sock = data[i];
@@ -8,17 +24,8 @@ function nonUniqueElements(data) {
 	};
 	return data.filter(() => {return true});
 }
+*/
 
 /*
 nonUniqueElements = a => a.filter(e => a.indexOf(e)!= a.lastIndexOf(e));
 */
-
-var assert = require('assert');
-
-if (!global.is_checking) {
-    assert.deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3], "1st example");
-    assert.deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), [], "2nd example");
-    assert.deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5], "3rd example");
-    assert.deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9], "4th example");
-    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
-}
