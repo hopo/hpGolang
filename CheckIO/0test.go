8cbd8ab3be11982db99f5c2e5c6f433000f10e01
hopo <hpsight@gmail.com>
2017-04-02 01:43:51 +0900

    -

--- CheckIO/0test.go
+++ CheckIO/0test.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"sort"
+	// "fmt"
+	// "sort"
 )
 
 func main() {
 	a := []int{10, 9, 10, 10, 9, 8}
 	x := 10
-
-	i := sort.Search(len(a), func(i int) bool { return a[i] >= x })
-	if i < len(a) && a[i] == x {
-		fmt.Printf("found %d at index %d in %v\n", x, i, a)
-	} else {
-		fmt.Printf("%d not found in %v\n", x, a)
-	}
 }
