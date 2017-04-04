979e8586d20772c3a85a72af1d6f639deb967e2b
hopo <hpsight@gmail.com>
2017-04-04 19:08:04 +0900

    find!! error and defer

--- CheckIO/0test.go
+++ CheckIO/0test.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"fmt"
+    // "log"
+    // "os"
     // "time"
 )
 
-type Data struct {
-    rmcm, rmco, rmcp, rocm, rocp, rpcm, rpco, rpcp int
+func echk(s []int, n int) int {
+    defer func() {
+        if err := recover(); err != nil {
+            return
+        }
+    }()
+    return s[n]
+}
+func main() {
+    a := echk([]int{1,2,3}, 1)
+    fmt.Println(a)
+}
+/*
+type MyError struct{
+    what string
 }
 
-func main() {
-    d := Data{}
-    fmt.Println(d)
+func (e MyError) Error() string {
+    return "Fucking-Error"
 }
+
+func run() (bl bool, er error) {
+    bl = 1 < 0
+    er = MyError{ "a" }
+    return
+}
+func main(){
+    b, err := run()
+    if err != nil {
+        fmt.Println(b, err)
+    }
+}
+*/
 
 // func countNeighbours(d Data, r int, c int) {
 //     fmt.Println(d.data[0], r, c)
