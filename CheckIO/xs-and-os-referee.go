3fe135121860e3b763dc4f37fbbcf09535388bd4
hopo <hpsight@gmail.com>
2017-04-07 01:36:00 +0900

    Solved CIO/ xs-and-os-refereee but..

--- CheckIO/xs-and-os-referee.go
+++ CheckIO/xs-and-os-referee.go
@@ -4,40 +4,36 @@ import (
     "fmt"
 )
 
-func xoReferee(data []string) interface{} {
+func xoReferee(data []string) string {
     var dt []string
     for i := 0; i < 3; i++ {
         for j := 0; j <3; j++ {
             dt = append(dt, string(data[i][j]))
         }
     }
-    var os, xs int
-    fmt.Println(dt)
+    var os, xs, dot int
     for _, d := range dt {
-        if "." == d {
-            if "O" == d {
-                os++
-            } else if "X" == d {
-                xs++            
-            }        
-        }
+        if "O" == d { os++ }
+        if "X" == d { xs++ }
+        if "." == d { dot++ }
     }
+    if dot == 0 { return "D" }
     if os > xs { return "O" }
     if os < xs { return "X" }
-    return "center"
+    return dt[4]
 }
 
 func main() {
-    // fmt.Println(xoReferee([]string{"X.O",
-    //                                "XX.",
-    //                                "XOO"})) //"X", "Xs wins"
+    fmt.Println(xoReferee([]string{"X.O",
+                                   "XX.",
+                                   "XOO"})) //"X", "Xs wins"
     fmt.Println(xoReferee([]string{"OO.",
                                    "XOX",
                                    "XOX"}))	//"O", "Os wins"
     fmt.Println(xoReferee([]string{"OOX",
                                    "XXO",
                                    "OXX"})) //"D", "Draw"
-    // fmt.Println(xoReferee([]string{"O.X",
-    //                                "XX.",
-    //                                "XOO"})) //"X", "Xs wins again"
+    fmt.Println(xoReferee([]string{"O.X",
+                                   "XX.",
+                                   "XOO"})) //"X", "Xs wins again"
 }
