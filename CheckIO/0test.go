b396ebbdd7e01d6282851afa165c5934013544bd
hopo <hpsight@gmail.com>
2017-04-06 23:08:03 +0900

    +

--- CheckIO/0test.go
+++ CheckIO/0test.go
@@ -8,15 +8,18 @@ import (
     // "time"
 )
 
+/*
 func main() {
     str := "abcdddfg"
 
-    // x := string([]rune(str)[0]) //UTF-8
-    x := string(str[3])  //ASCII
+    // n := string([]rune(str)[0]) //UTF-8
+    n := string(str[3])  //ASCII
 
-    i := strings.Count(str, x)
-    fmt.Println(x, i)
+    i := strings.Count(str, n)
+    fmt.Println(n, i)
 }
+*/
+
 /*
 func chcker(data [][]int, r int, c int) int {
     defer func() {
--- CheckIO/xs-and-os-referee.go
+++ CheckIO/xs-and-os-referee.go
@@ -4,24 +4,40 @@ import (
     "fmt"
 )
 
-func xoReferee(data []string) string {
-    return "D"
+func xoReferee(data []string) interface{} {
+    var dt []string
+    for i := 0; i < 3; i++ {
+        for j := 0; j <3; j++ {
+            dt = append(dt, string(data[i][j]))
+        }
+    }
+    var os, xs int
+    fmt.Println(dt)
+    for _, d := range dt {
+        if "." == d {
+            if "O" == d {
+                os++
+            } else if "X" == d {
+                xs++            
+            }        
+        }
+    }
+    if os > xs { return "O" }
+    if os < xs { return "X" }
+    return "center"
 }
 
 func main() {
-    fmt.Println(xoReferee([]string{"X.O",
-                                   "XX.",
-                                   "XOO"})) //"X", "Xs wins"
-
+    // fmt.Println(xoReferee([]string{"X.O",
+    //                                "XX.",
+    //                                "XOO"})) //"X", "Xs wins"
     fmt.Println(xoReferee([]string{"OO.",
                                    "XOX",
                                    "XOX"}))	//"O", "Os wins"
-
     fmt.Println(xoReferee([]string{"OOX",
                                    "XXO",
                                    "OXX"})) //"D", "Draw"
-
-    fmt.Println(xoReferee([]string{"O.X",
-                                   "XX.",
-                                   "XOO"})) //"X", "Xs wins again"
+    // fmt.Println(xoReferee([]string{"O.X",
+    //                                "XX.",
+    //                                "XOO"})) //"X", "Xs wins again"
 }
