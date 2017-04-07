6a86ee5f263413f4284d175dde04ee1d2d8b1620
hopo <hpsight@gmail.com>
2017-04-07 14:36:30 +0900

    CIO/ xs-and-os-referee another solution

--- CheckIO/Home/xs-and-os-referee2.go
+++ CheckIO/Home/xs-and-os-referee2.go
@@ -5,27 +5,26 @@ import (
 )
 
 func xoReferee(data []string) string {
-    x, y, z := data[0], data[1], data[2]
+	const i int = 0
+	x, y, z := data[i], data[i+1], data[i+2]
 
-    // for i := 0; i < 3; i++ {
-    //     if x[i] == y[i] && y[i] == z[i] { return string(x[i]) }
-    //     for _, D := range []string{x, y, z} {
-    //         if D[0] == D[1] && D[1] == D[2] { return string(D[0]) }
-    //     }
-    // }
-    //
-    // if x[0] == y[1] && y[1] == z[2] { return string(x[0]) }
-    // if x[2] == y[1] && y[1] == z[0] { return string(z[0]) }
+	if x[i] == y[i+1] && y[i+1] == z[i+2] {
+		return string(x[i])
+	}
+	if x[i+2] == y[i+1] && y[i+1] == z[i] {
+		return string(x[i+2])
+	}
 
-    for i, D := range []string{x, y, z} {
-        if D[0] == D[1] && D[1] == D[2] { return string(D[0]) }
-        if x[i] == y[i] && y[i] == z[i] { return string(x[i]) }
+	for j, D := range []string{x, y, z} {
+		if D[i] == D[i+1] && D[i+1] == D[i+2] {
+			return string(D[i])
+		}
+		if x[j] == y[j] && y[j] == z[j] {
+			return string(x[j])
+		}
+	}
 
-        if x[0] == y[1] && y[1] == z[2] { return string(x[0]) }
-        if x[2] == y[1] && y[1] == z[0] { return string(z[0]) }
-    }
-
-    return "D"
+	return "D"
 }
 
 func main() {
