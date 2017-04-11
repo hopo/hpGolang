63b281e33c801a786425829cccfb36199d46238c
hopo <hpsight@gmail.com>
2017-04-11 15:36:08 +0900

    -

--- webApplication/main.go
+++ webApplication/main.go
@@ -7,18 +7,26 @@ import (
 	"net/http"
 )
 
+type city struct {
+	Num   int
+	Title string
+	Desc  string
+}
+
 func main() {
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/index", indexHandler)
+	// http.HandleFunc("/index", indexHandler)
 	http.HandleFunc("/topic", topicHandler)
+	// http.HandleFunc("/topic/:Num", topicIdHandler)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":4000", nil)
 }
 
 func rootHandler(res http.ResponseWriter, rq *http.Request) {
-	io.WriteString(res, `<a href="/index">Enter</a>`)
+	io.WriteString(res, `<a href="/topic">Enter Topic</a>`)
 }
 
+/*
 func indexHandler(res http.ResponseWriter, rq *http.Request) {
 	tpl, err := template.ParseFiles("doc/index.gohtml")
 	if err != nil {
@@ -26,12 +34,7 @@ func indexHandler(res http.ResponseWriter, rq *http.Request) {
 	}
 	tpl.Execute(res, nil)
 }
-
-//City struct
-type City struct {
-	Name string
-	Desc string
-}
+*/
 
 func topicHandler(res http.ResponseWriter, rq *http.Request) {
 	tpl, err := template.ParseFiles("doc/topic.gohtml")
@@ -39,13 +42,13 @@ func topicHandler(res http.ResponseWriter, rq *http.Request) {
 		log.Fatalln(err)
 	}
 
-	seoul := City{"Seoul", "Kyeongki"}
-	busan := City{"Busan", "KyeonNam"}
-	daejeon := City{"Daejeon", "ChungNam"}
-	osaka := City{"Osaka", "Japan-Asia"}
-	vancouver := City{"Vancouver", "Canada-NorthAmerica"}
+	seoul := city{1, "Seoul", "Kyeongki"}
+	busan := city{2, "Busan", "KyeonNam"}
+	daejeon := city{3, "Daejeon", "ChungNam"}
+	osaka := city{4, "Osaka", "Japan-Asia"}
+	vancouver := city{5, "Vancouver", "Canada-NorthAmerica"}
 
-	c := []City{seoul, busan, daejeon, osaka, vancouver}
+	c := []city{seoul, busan, daejeon, osaka, vancouver}
 
 	tpl.ExecuteTemplate(res, "topic.gohtml", c)
 }
