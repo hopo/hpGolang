f84512dabe136f78d2452f822d7baaa149861362
hopo <hpsight@gmail.com>
2017-05-16 21:39:42 +0900

    .

--- LearnGo_ALee/collectionMap2.go
+++ LearnGo_ALee/collectionMap2.go
@@ -1,18 +1,18 @@
 package main
 
 func main() {
-    tickers := map[string]string{
-        "GOOG": "Google Inc",
-        "MSFT": "Microsoft",
-        "FB":   "FaceBook",
-        "AMZN": "Amazon",
-    }
+	tickers := map[string]string{
+		"GOOG": "Google Inc",
+		"MSFT": "Microsoft",
+		"FB":   "FaceBook",
+		"AMZN": "Amazon",
+	}
 
-    // map 키 유무 체크 
-    val, exists := tickers["MSFT"] // key value, bool value
-    if !exists {
-        println("No MSFT ticker")
-    } else {
+	// map 키 유무 체크
+	val, exists := tickers["MSFT"] // key value, bool value
+	if !exists {
+		println("No MSFT ticker")
+	} else {
 		println(val)
 	}
 }
