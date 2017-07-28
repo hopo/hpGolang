2fa59b9b55db7f2ac19986e2cf48ed3194205d6c
hopo <hpsight@gmail.com>
2017-07-28 16:06:20 +0900

    ~

--- GTE/GolangTraining/02_package/main/main.go
+++ GTE/GolangTraining/02_package/main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"GTE/GolangTraining/02_package/icomefromalaska"
+	"GTE/GolangTraining/02_package/stringutil"
 	"fmt"
-	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	//"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	//"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
