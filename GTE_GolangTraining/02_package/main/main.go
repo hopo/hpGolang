de934f1e627c7bdafeddc0e86b2bded958872ac1
hopo <hpsight@gmail.com>
2017-07-30 16:09:15 +0900

    go build!

--- GTE_GolangTraining/02_package/main/main.go
+++ GTE_GolangTraining/02_package/main/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"GTE/GolangTraining/02_package/icomefromalaska"
-	"GTE/GolangTraining/02_package/stringutil"
 	"fmt"
-	//"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
-	//"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
---
+++ GTE_GolangTraining/04_scope/04_variable-shadowing/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func max(x int) int {
+	return 42 + x
+}
+
+func main() {
+	max := max(7)
+	fmt.Println(max) // max is now the result, not the function
+}
+
+// don't do this; bad conding practice to shadow variable
