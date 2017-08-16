dad09e266aca14b2a31e21bc7019d798c566e198
hopo <hpsight@gmail.com>
2017-08-17 01:02:32 +0900

    absolute-sorting/03/

--- CheckIO/01_Elementary/10_absolute-sorting/03/main.go
+++ CheckIO/01_Elementary/10_absolute-sorting/03/main.go
@@ -3,31 +3,41 @@ package main
 import (
 	"fmt"
 	"math"
-	//"sort"
 )
 
-// ByAbs struct
 type ByAbs []int
 
-func (p ByAbs) Len() int           { return len(p) }
-func (p ByAbs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-func (p ByAbs) Less(i, j int) bool { return math.Abs(float64(p[i])) < math.Abs(float64(p[j])) }
+func (p ByAbs) Len() int {
+	return len(p)
+}
 
-func absoluteSorting(is []int) []int {
-	var p ByAbs = is
+func (p ByAbs) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
 
-	fmt.Println(p.Less(p[1], p[0]))
+func (p ByAbs) Less(i, j int) bool {
+	return math.Abs(float64(p[i])) < math.Abs(float64(p[j]))
+}
+
+func (p ByAbs) absoluteSorting() []int {
+	l := p.Len()
+	for i := 1; i < l; i++ {
+		for j := 0; j < l-1; j++ {
+			if p.Less(i, j) {
+				p.Swap(i, j)
+			}
+		}
+	}
 	return p
 }
 
 func main() {
-	fmt.Println(absoluteSorting([]int{-20, -5, 10, 15})) //[-5, 10, 15, -20]
-	//fmt.Println(absoluteSorting([]int{1, 2, 3, 0}))      //[0, 1, 2, 3]
-	//fmt.Println(absoluteSorting([]int{-1, -2, -3, 0}))   //[0, -1, -2, -3]
-}
+	var p1, p2, p3 ByAbs
+	p1 = []int{-20, -5, 10, 15}
+	p2 = []int{1, 2, 3, 0}
+	p3 = []int{-1, -2, -3, 0}
 
-/*
-function absoluteSorting(numbers){
-	return numbers.sort(function(a, b){return Math.abs(a)-Math.abs(b);});
+	fmt.Println(p1.absoluteSorting()) //[-5, 10, 15, -20]
+	fmt.Println(p2.absoluteSorting()) //[0, 1, 2, 3]
+	fmt.Println(p3.absoluteSorting()) //[0, -1, -2, -3]
 }
-*/
