3952deca5c8b8b37a4708f994b3caf239d5be25b
hopo <hpsight@gmail.com>
2017-08-18 21:10:44 +0900

    quiz square root

---
+++ ex/quizSqrt/main.go
@@ -0,0 +1,37 @@
+// Go's `math/rand` package provides
+// [pseudorandom number](http://en.wikipedia.org/wiki/Pseudorandom_number_generator)
+// generation.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func main() {
+	nsrc := rand.NewSource(time.Now().UnixNano())
+	rdm := rand.New(nsrc)
+
+	for {
+		num := rdm.Intn(100)
+		fmt.Println()
+		fmt.Print(num*num, " ???  ")
+
+		var a string
+		fmt.Scanln(&a)
+
+		v, _ := strconv.Atoi(a)
+
+		switch {
+		case a == "q":
+			return
+		case num == v:
+			fmt.Println("Good Job!")
+		case num != v:
+			fmt.Println("no: answer is ", num)
+		}
+	}
+}
