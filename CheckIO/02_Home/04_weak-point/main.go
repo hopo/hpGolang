ce32e26f2e4802ff58b7b754e18627d405f9b676
hopo <hpsight@gmail.com>
2017-08-19 16:26:00 +0900

    CIO/02_Home/04_weak-point/

--- CheckIO/02_Home/04_weak-point/main.go
+++ CheckIO/02_Home/04_weak-point/main.go
@@ -1,35 +1,80 @@
-// solve it!
-
-
 package main
 
 import "fmt"
 
-"use strict";
+type Matrix [][]int
 
-function weakPoint(matrix){
-    return [0, 0]
+func (m Matrix) rowSum() []int {
+	var rslt []int
+	for _, v := range m {
+		var s int
+		for _, vv := range v {
+			s += vv
+		}
+		rslt = append(rslt, s)
+	}
+	return rslt
 }
 
-var assert = require('assert');
+func (m Matrix) columnSum() []int {
+	var rslt []int
+	for i, v := range m {
+		var s int
+		for j, _ := range v {
+			s += m[j][i]
+		}
+		rslt = append(rslt, s)
+	}
+	return rslt
+}
 
-if (!global.is_checking) {
-    assert.deepEqual(weakPoint([[7, 2, 7, 2, 8],
-                                [2, 9, 4, 1, 7],
-                                [3, 8, 6, 2, 4],
-                                [2, 5, 2, 9, 1],
-                                [6, 6, 5, 4, 5]]
-                                ), [3, 3], "Example");
-    assert.deepEqual(weakPoint([[7, 2, 4, 2, 8],
-                                [2, 8, 1, 1, 7],
-                                [3, 8, 6, 2, 4],
-                                [2, 5, 2, 9, 1],
-                                [6, 6, 5, 4, 5]]
-                                ), [1, 2], "Two weak point");
+func lowerValueIndex(is []int) int {
+	var c int = is[0]
+	var rslt int
+	for i, _ := range is {
+		if is[i] < c {
+			c = is[i]
+			rslt = i
+		}
+	}
+	return rslt
+}
 
-    assert.deepEqual(weakPoint([[1, 1, 1],
-                                [1, 1, 1],
-                                [1, 1, 1]]
-                                ), [0, 0], "Top left");
+func (m Matrix) weakPoint() []int {
+	rs := m.rowSum()
+	cs := m.columnSum()
 
+	lvi1 := lowerValueIndex(rs)
+	lvi2 := lowerValueIndex(cs)
+
+	rslt := []int{lvi1, lvi2}
+
+	return rslt
+}
+
+func main() {
+	m1 := Matrix{
+		{7, 2, 7, 2, 8},
+		{2, 9, 4, 1, 7},
+		{3, 8, 6, 2, 4},
+		{2, 5, 2, 9, 1},
+		{6, 6, 5, 4, 5},
+	}
+	m2 := Matrix{
+		{7, 2, 4, 2, 8},
+		{2, 8, 1, 1, 7},
+		{3, 8, 6, 2, 4},
+		{2, 5, 2, 9, 1},
+		{6, 6, 5, 4, 5},
+	}
+	m3 := Matrix{
+		{1, 1, 1},
+		{1, 1, 1},
+		{1, 1, 1},
+	}
+	fmt.Println(
+		m1.weakPoint(), // {3 3}, "Example"
+		m2.weakPoint(), // {1 2}, "Two weak point"
+		m3.weakPoint(), // {0 0}, "Top left"
+	)
 }
