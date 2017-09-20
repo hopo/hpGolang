c9d6636d72b3494188f476f635ad6ec41e51ed72
hopo <hpsight@gmail.com>
2017-09-21 00:48:41 +0900

    ~

--- CheckIO/03_Electronic-Station/02_the-hamming-distance/01/main.go
+++ CheckIO/03_Electronic-Station/02_the-hamming-distance/01/main.go
@@ -9,33 +9,47 @@ func main() {
 	//fmt.Println(hammingDistance(117, 17)) //3, "First example"
 	//fmt.Println(hammingDistance(1, 2))    //2, "Second example"
 	//fmt.Println(hammingDistance(16, 15))  //5, "Third example"
-	fmt.Println(hammingDistance(117, 17)) // "hp ex""
+	fmt.Println(hammingDistance(1, 2)) // "hp ex""
 }
 
 func hammingDistance(n, m int) int {
-	x := ntobConverter(n)
-	y := ntobConverter(m)
+	x := itotypebConverter(n)
+	y := itotypebConverter(m)
 
 	z := x - y
 
-	var ret int
-	ret = btonConverter(z)
+	fmt.Println("x, y, z:", x, y, z)
 
+	ss := strconv.Itoa(z)
+	var ii int
+	for _, v := range ss {
+		c, _ := strconv.Atoi(string(v))
+		ii += c
+	}
+
+	var ret int = ii
 	return ret
 }
 
-func ntobConverter(n int) int {
+func itotypebConverter(n int) int {
 	//s := fmt.Sprintf("%08b", n)
 	s := strconv.FormatInt(int64(n), 2)
+	fmt.Println("n:", s)
 
 	i, _ := strconv.Atoi(s)
 	return i
 }
 
-func btonConverter(n int) int {
-	s := strconv.FormatInt(int64(n), 10)
-	i, _ := strconv.Atoi(s)
-	fmt.Println("bton", i)
+/*
 
-	return i
+func typebtoiConverter(n int) int {
+	s := strconv.Itoa(n)
+
+	var ii int
+	for i := len(s) - 1; i > -1; i-- {
+		ii += int(math.Pow(2, float64(i)))
+	}
+	var ret int = ii
+	return ret
 }
+*/
