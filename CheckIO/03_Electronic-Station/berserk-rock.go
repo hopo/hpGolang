56ab49e3b6791722c0f4f95a457b05241badaa54
hopo <hpsight@gmail.com>
2017-10-01 02:04:42 +0900

    ~

--- CheckIO/03_Electronic-Station/berserk-rock.go
+++ CheckIO/03_Electronic-Station/berserk-rock.go
@@ -10,9 +10,7 @@ func berserkRook(marbles string, step []string) interface{} {
 }
 
 func main() {
-	fmt.Println(
-		berserkRook("d3", []string{"d6", "b6", "c8", "g4", "b8", "g6"}), //5, "First"
-		berserkRook("a2", []string{"f6", "f2", "a6", "f8", "h8", "h6"}), //6, "Second"
-		berserkRook("a2", []string{"f6", "f8", "f2", "a6", "h6"}),       //4, "Third"
-	)
+	fmt.Println(berserkRook("d3", []string{"d6", "b6", "c8", "g4", "b8", "g6"})) //5, "First"
+	fmt.Println(berserkRook("a2", []string{"f6", "f2", "a6", "f8", "h8", "h6"})) //6, "Second"
+	fmt.Println(berserkRook("a2", []string{"f6", "f8", "f2", "a6", "h6"}))       //4, "Third"
 }
--- CheckIO/03_Electronic-Station/climbing-route.go
+++ CheckIO/03_Electronic-Station/climbing-route.go
@@ -1,51 +1,50 @@
-"use strict";
+package main
 
-function climbingRoute(elevation_map){
-    return 0;
+import "fmt"
+
+func climbingRoute(elevation_map []string) (ret int) {
+	return
 }
 
-var assert = require('assert');
-
-if (!global.is_checking) {
-    assert.equal(climbingRoute([
-        '000',
-        '210',
-        '000']), 6, 'basic')
-    assert.equal(climbingRoute([
-        '00000',
-        '05670',
-        '04980',
-        '03210',
-        '00000']), 26, 'spiral')
-    assert.equal(climbingRoute([
-        '000000001',
-        '222322222',
-        '100000000']), 26, 'bridge')
-    assert.equal(climbingRoute([
-        '000000002110',
-        '011100002310',
-        '012100002220',
-        '011100000000']), 26, 'two top')
-    assert.equal(climbingRoute([
-        '000000120000',
-        '001002432100',
-        '012111211000',
-        '001000000000']), 16, 'one top')
-    assert.equal(climbingRoute([
-        '00000000111111100',
-        '00000000122222100',
-        '00000000123332100',
-        '00000000123432100',
-        '00000000123332100',
-        '00000000122222100',
-        '00000000111111100',
-        '00011111000000000',
-        '00012221000000000',
-        '00012321000000000',
-        '00012221000000012',
-        '00011111000000000',
-        '11100000000000000',
-        '12100000000000000',
-        '11100000000000000']), 52, 'pyramids')
-    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+func main() {
+	fmt.Println(climbingRoute([]string{
+		"000",
+		"210",
+		"000"})) // 6, 'basic'
+	fmt.Println(climbingRoute([]string{
+		"00000",
+		"05670",
+		"04980",
+		"03210",
+		"00000"})) // 26, 'spiral'
+	fmt.Println(climbingRoute([]string{
+		"000000001",
+		"222322222",
+		"100000000"})) // 26, 'bridge'
+	fmt.Println(climbingRoute([]string{
+		"000000002110",
+		"011100002310",
+		"012100002220",
+		"011100000000"})) // 26, 'two top'
+	fmt.Println(climbingRoute([]string{
+		"000000120000",
+		"001002432100",
+		"012111211000",
+		"001000000000"})) // 16, 'one top'
+	fmt.Println(climbingRoute([]string{
+		"00000000111111100",
+		"00000000122222100",
+		"00000000123332100",
+		"00000000123432100",
+		"00000000123332100",
+		"00000000122222100",
+		"00000000111111100",
+		"00011111000000000",
+		"00012221000000000",
+		"00012321000000000",
+		"00012221000000012",
+		"00011111000000000",
+		"11100000000000000",
+		"12100000000000000",
+		"11100000000000000"})) // 52, 'pyramids'
 }
