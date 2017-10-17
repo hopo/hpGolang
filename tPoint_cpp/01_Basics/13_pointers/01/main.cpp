01398a99262f7c9bf81b30b337ff17701e54bb1a
hopo <hpsight@gmail.com>
2017-10-17 20:17:02 +0900

    c: pointers

---
+++ tPoint_cpp/01_Basics/13_pointers/01/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int var1;
+	char var2[10];
+
+	cout << "Address of var1 variable: ";
+	cout << &var1 << endl;
+
+	cout << "Address of var2 variable: ";
+	cout << &var2 << endl;
+
+	return 0;
+}
---
+++ tPoint_cpp/01_Basics/13_pointers/03_null-pointers/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int *ptr = NULL;
+	cout << "The value fo ptr is " << ptr;
+
+	return 0;
+}
