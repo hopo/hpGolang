b29c48899184e1e2317d4984fde755a29fcd8571
hopo <hpsight@gmail.com>
2017-10-27 14:36:33 +0900

    py: CIO

--- CheckIO_py/number-radix.py
+++ CheckIO_py/number-radix.py
@@ -1,6 +1,20 @@
+ALP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def checkio(str_number, radix):
-    n = -1
-    print(n)
+    l = len(str_number)
+    il = []
+    for i in range(l):
+        if str_number[i] in ALP:
+            e = ALP.index(str_number[i]) + 10
+        else:
+            e = int(str_number[i])
+        if e >= radix:
+            return -1
+        else:
+            r = e * radix**(l-(i+1))
+            il.append(r)
+    return sum(il)
 
 if __name__ == '__main__':
-    checkio("AF", 16)
+    ex = checkio("AF", 16)
+    print(ex)
