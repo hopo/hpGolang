a36f05960ec6367f21ef81dea4bd2fbad0faf5d1
hopo <hpsight@gmail.com>
2017-11-02 10:27:08 +0900

    CIO

---
+++ CheckIO/01_Elementary/11_the-most-frequent/q.py
@@ -0,0 +1,19 @@
+// https://py.checkio.org/mission/the-most-frequent/
+
+def most_frequent(data):
+    """
+        determines the most frequently occurring string in the sequence.
+    """
+    # your code here
+    return None
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert most_frequent([
+        'a', 'b', 'c',
+        'a', 'b',
+        'a'
+    ]) == 'a'
+
+    assert most_frequent(['a', 'a', 'bi', 'bi', 'bi']) == 'bi'
+    print('Done')
