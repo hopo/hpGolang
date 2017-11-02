3c93c6cc83ba9fef6811377c95bd1a51379e2a62
hopo <hpsight@gmail.com>
2017-11-02 12:29:50 +0900

    CIO-Lv1

---
+++ CheckIO/01_Elementary/08_digits-multiplication/q.py
@@ -0,0 +1,14 @@
+# https://py.checkio.org/mission/digits-multiplication/
+
+def checkio(number):
+
+    return 1
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
