de612d757079bd2dff540e27287ebc4474ee6587
hopo <hpsight@gmail.com>
2017-11-05 16:00:53 +0900

    Palgo

--- Programmers-algo/01_level1/sort_dictionary/01/main.go
+++ Programmers-algo/01_level1/sort_dictionary/01/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	ncard := map[string]int{"김철수": 78, "이하나": 97, "정진원": 88}
-	fmt.Println(ncard["김철수"])
+	ex1 := sort_dictionary(ncard)
+	fmt.Println(ex1)
+}
+
+type Ncard struct {
+	name   string
+	number int
+}
+
+func sort_dictionary(nc map[string]int) (ret []Ncard) {
+	var slis []string
+	for k, _ := range nc {
+		slis = append(slis, k)
+	}
+	sort.Strings(slis)
+	for _, k := range slis {
+		ret = append(ret, Ncard{k, nc[k]})
+	}
+	return
 }
