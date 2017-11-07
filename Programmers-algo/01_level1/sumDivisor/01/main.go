95e3180f5161cee2b4322b19bb3743537d25fa01
hopo <hpsight@gmail.com>
2017-11-07 21:56:36 +0900

    Palgo

---
+++ Programmers-algo/01_level1/sumDivisor/01/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hopo/hpkg"
+)
+
+func main() {
+	ex1 := sumDivisor(12) // 28
+	ex2 := sumDivisor(13) // 14
+	fmt.Println(ex1)
+	fmt.Println(ex2)
+}
+
+func sumDivisor(i int) (ret int) {
+	d := hpkg.Denom(i)
+	ret = hpkg.SumIsl(d)
+	return
+}
