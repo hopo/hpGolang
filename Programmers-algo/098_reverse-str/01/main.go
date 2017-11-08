920cfb11aed80f1a0899917d72aba6773d19135c
hopo <hpsight@gmail.com>
2017-11-08 23:55:41 +0900

    ~

---
+++ Programmers-algo/098_reverse-str/01/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ex := reverseStr("Zbcdefg")
+	fmt.Println(ex)
+}
+
+func reverseStr(s string) (ret string)
+
+/*
+// https://programmers.co.kr/learn/challenge_codes/98
+
+public class ReverseStr {
+	public String reverseStr(String str){
+
+		return "";
+	}
+
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
+		ReverseStr rs = new ReverseStr();
+		System.out.println( rs.reverseStr("Zbcdefg") );
+	}
+}
+*/
