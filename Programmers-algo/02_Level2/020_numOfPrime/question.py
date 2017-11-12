889e309b1aa8df3a1bbd54495dcf7609d2247458
hopo <hpsight@gmail.com>
2017-11-12 18:59:37 +0900

    ~

---
+++ Programmers-algo/02_Level2/020_numOfPrime/question.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/challenge_codes/20
+
+def numberOfPrime(n):
+    # 1부터 n사이의 소수는 몇 개인가요?
+    return 0
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(numberOfPrime(10))
---
+++ Programmers-algo/02_Level2/182_getMinSum/question.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/challenge_codes/182
+
+def getMinSum(A,B):
+    answer = 0
+
+    return answer
+
+#아래 코드는 출력을 위한 테스트 코드입니다.
+
+print(getMinSum([1,2],[3,4]))
