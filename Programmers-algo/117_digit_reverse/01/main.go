7a99f06e4a0847033aa105063c9694c771e61630
hopo <hpsight@gmail.com>
2017-11-14 16:23:09 +0900

    Palgo

--- Programmers-algo/117_digit_reverse/01/main.go
+++ Programmers-algo/117_digit_reverse/01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/hopo/hpkg"
 )
 
 func main() {
@@ -9,7 +10,15 @@ func main() {
 	fmt.Println(ex)
 }
 
-func digit_reverse(n int) []int
+func digit_reverse(n int) []int {
+	isl := hpkg.Int_convert_each(n)
+	var ret []int
+	for i := 0; i < len(isl); i++ {
+		j := len(isl) - 1 - i
+		ret = append(ret, isl[j])
+	}
+	return ret
+}
 
 /*
 # https://programmers.co.kr/learn/challenge_codes/117
