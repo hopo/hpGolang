d944fa9f3c3b039a4cf7ea46c18de858f157457a
hopo <hpsight@gmail.com>
2017-11-15 19:53:26 +0900

    Palgo Level2

--- Programmers-algo/115_toWeirdCase/01/main.go
+++ Programmers-algo/115_toWeirdCase/01/main.go
@@ -5,11 +5,33 @@ import (
 )
 
 func main() {
-	ex := toWeirdCase("try hello world") // "TrY HeLlO WoRlD"
-	fmt.Println(ex)
+	ex1 := toWeirdCase("try hello world")               // "TrY HeLlO WoRlD"
+	ex2 := toWeirdCase("coding is thinking not typing") // "CoDiNg Is ThInKiNg NoT TyPiNg"
+	fmt.Println(ex1)
+	fmt.Println(ex2)
 }
 
 func toWeirdCase(s string) string {
+	slsl := split_by_blank(s)
+	for _, v := range slsl {
+		for i, _ := range v {
+			if i%2 == 0 {
+				v[i] = minus_32(v[i])
+			}
+		}
+	}
+	var ret string
+	for i, _ := range slsl {
+		if i == len(slsl)-1 {
+			ret += string(slsl[i])
+		} else {
+			ret += string(slsl[i]) + " "
+		}
+	}
+	return ret
+}
+
+func split_by_blank(s string) [][]byte {
 	smpl := []byte(s)
 	var ind []int
 	for i, _ := range smpl {
@@ -17,31 +39,25 @@ func toWeirdCase(s string) string {
 			ind = append(ind, i)
 		}
 	}
-	var x, y, z []byte
-	fmt.Println(smpl)
-	fmt.Println(ind)
-	x = smpl[:3]
-	y = smpl[3:9]
-	z = smpl[9:]
-	fmt.Println(x, y, z)
-	return ""
+
+	var ret [][]byte
+	var box []byte
+	a, b := 0, ind[0]
+	box = smpl[:b]
+	ret = append(ret, box)
+	for i := 0; i < len(ind); i++ {
+		if i == len(ind)-1 {
+			box = smpl[b+1:]
+			ret = append(ret, box)
+			break
+		}
+		a, b = ind[i], ind[i+1]
+		box = smpl[a+1 : b]
+		ret = append(ret, box)
+	}
+	return ret
 }
 
-/*
-#https://programmers.co.kr/learn/challenge_codes/115
-
-"""
-toWeirdCase함수는 문자열 s를 매개변수로 입력받습니다.
-문자열 s에 각 단어의 짝수번째 인덱스 문자는 대문자로, 홀수번째 인덱스 문자는 소문자로 바꾼 문자열을 리턴하도록 함수를 완성하세요.
-예를 들어 s가 "try hello world"라면 첫 번째 단어는 "TrY", 두 번째 단어는 "HeLlO", 세 번째 단어는 "WoRlD"로 바꿔 "TrY HeLlO WoRlD"를 리턴하면 됩니다.
-
-주의 문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단합니다.
-"""
-
-def toWeirdCase(s):
-    # 함수를 완성하세요
-    return ''
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print("결과 : {}".format(toWeirdCase("try hello world")));
-*/
+func minus_32(b byte) byte {
+	return b - 32
+}
