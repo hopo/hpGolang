11f185f40d7eb0b934a186e05f8ad211ad68d668
hopo <hpsight@gmail.com>
2017-11-20 12:39:28 +0900

    variety3

--- Programmers-algo/api_lab.go
+++ Programmers-algo/api_lab.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	//fmt.Println(binary_convert(78)) // 1001110
-	make_vari([]string{"0", "1", "2"})
+	make_vari([]string{"a", "b", "c"})
 }
 
 // variety
@@ -20,34 +20,29 @@ func make_vari(ssl []string) {
 func variety3(ssl []string) []string {
 	var fix string
 	var ret, box []string
-	box = append(box, ssl...)
-	lnth := len(box)
+	lnth := len(ssl)
+	//	box = make([]string, lnth)
+	//	copy(box, ssl)
+	box = ssl
+
 	for i := 0; i < lnth; i++ {
-		fix = box[i]
-		box[i] = ""
+		fix, box = box[0], box[1:] // set: fix, box
+
 		var send []string
-		for _, v := range box {
-			if v != "" {
-				send = append(send, v)
-			}
-		}
-
+		send = make([]string, len(box))
+		copy(send, box)
 		vr2 := variety2(send)
-		var s string
-		var rcv []string
+
+		var rcv string
 		for _, v := range vr2 {
-			s += v
-			rcv = append(rcv, s)
-			s = ""
+			rcv = fix + v
+			ret = append(ret, rcv)
+			rcv = ""
 		}
 
-		for _, v := range rcv {
-			s = fix + v
-			ret = append(ret, s)
-			s = ""
-		}
-		box[i] = fix
+		box = append(box, fix) // init: fix, box
 	}
+
 	return ret
 }
 
@@ -65,35 +60,4 @@ func variety2(ssl []string) []string {
 	}
 	ret = append(ret, s)
 	return ret
-}
-
-// input int n, convert (type string)binary
-func binary_convert(n int) string {
-	//fmt.Println("n int:", n) // checker
-	//fmt.Printf("%b\n", n) // checker
-	var tsl []int
-	for i := 1; i < n+1; i *= 2 {
-		tsl = append([]int{i}, tsl...) // push front
-	}
-
-	zno := make([]int, len(tsl))
-	for i := 0; i < len(tsl); i++ {
-		n = n - tsl[i]
-		if n == 0 {
-			zno[i] = 1
-			break
-		}
-		if n > 0 {
-			zno[i] = 1
-		}
-		if n < 0 {
-			n = n + tsl[i]
-		}
-	}
-
-	var r string
-	for _, v := range zno {
-		r += string(v + 48) // int make string
-	}
-	return r
 }
