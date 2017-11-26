aa50b7eb841a1df09fdff366afc75b4af70dda17
hopo <hpsight@gmail.com>
2017-11-26 17:47:08 +0900

    Palgo Level4

--- Programmers-algo/04_Level4/035_hopscotch/01/main.go
+++ Programmers-algo/04_Level4/035_hopscotch/01/main.go
@@ -1,5 +1,3 @@
-// ...ing
-
 package main
 
 import (
@@ -7,34 +5,36 @@ import (
 )
 
 func main() {
-
-	brd1 := [][]int{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}} // 16, (5)+(7)+(4)
-	ex1 := hopscotch(brd1, 3)
+	board1 := [][]int{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}} // 16, (5)+(7)+(4)
+	ex1 := hopscotch(board1, 3)
 	fmt.Println(ex1)
+	board2 := [][]int{{1, 2, 7, 5}, {3, 2, 6, 4}, {2, 3, 4, 5}, {1, 1, 9, 1}} // 15, (7)+(4)+(4)+(1)
+	ex2 := hopscotch(board2, 4)
+	fmt.Println(ex2)
+}
+
+type Data struct {
+	sum   int
+	avoid int
 }
 
 func hopscotch(board [][]int, size int) int {
-	return -1
+	dt := Data{0, -1} // init
+	for i := 0; i < size; i++ {
+		dt = sum(dt, board[i])
+	}
+	return dt.sum
 }
 
-/*
-# https://programmers.co.kr/learn/challenge_codes/35
-
-"""
-영희는 땅따먹기 게임에 푹 빠졌습니다. 땅따먹기 게임의 땅은 총 N행 4열로 나누어져 있고, 모든 칸에는 점수가 쓰여 있습니다. 땅을 밟으면서 한 행씩 내려올 때, 영희는 각 행의 4칸 중 1칸만 밟으면서 내려올 수 있습니다. 땅따먹기 게임에는 같은 열을 연속해서 밟을 수가 없는 특수 규칙이 있습니다. 즉, 1행에서 (5)를 밟았다면, 2행의 (8)은 밟을 수가 없게 됩니다. 마지막 행까지 모두 내려왔을 때, 점수가 가장 높은 사람이 게임의 승자가 됩니다. 여러분이 hopscotch 함수를 제작하여 영희가 최대 몇 점을 얻을 수 있는지 알려주세요. 예를 들어
-1 2 3 5
-5 6 7 8
-4 3 2 1
-의 땅이 있다면, 영희는 각 줄에서 (5), (7), (4) 땅을 밟아 16점을 최고점으로 받을 수 있으며, hopscotch 함수에서는 16을 반환해주면 됩니다.
-"""
-
-def hopscotch(board, size):
-    result = 0
-    # 땅따먹기 게임으로 얻을 수 있는 최대 점수는?
-    return result
-
-
-#아래는 테스트로 출력해 보기 위한 코드입니다.
-board =  [[ 1, 2, 3, 5 ], [ 5, 6, 7, 8 ], [4, 3, 2, 1]]
-print(hopscotch(board, 3))
-*/
+func sum(dt Data, isl []int) Data {
+	var s, a int
+	for i, v := range isl {
+		if dt.avoid != i && s < v {
+			s = v
+			a = i
+		}
+	}
+	var ret Data
+	ret = Data{dt.sum + s, a}
+	return ret
+}
