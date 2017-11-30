10541ab308c73aa55d52e95604fb4bc8bd34054b
hopo <hpsight@gmail.com>
2017-11-30 11:53:34 +0900

    tPoint_cpp OOP

--- tPoint_cpp/02_Object-Oriented/02_inheritance/02_multiple-inheritance/main.cpp
+++ tPoint_cpp/02_Object-Oriented/02_inheritance/02_multiple-inheritance/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Base class
+// Base class Shape
 class Shape {
 	public:
 		void setWidth(int w) {
@@ -12,14 +12,21 @@ class Shape {
 			height = h;
 		}
 
-	
 	protected:
 		int width;
 		int height;
 };
 
+// Base class PaintCost
+class PaintCost {
+	public:
+		int getCost(int area) {
+			return area * 70; 
+		}
+};
+
 // Derived class
-class Rectangle: public Shape {
+class Rectangle: public Shape, public PaintCost {
 	public:
 		int getArea() {
 			return (width * height);
@@ -28,12 +35,18 @@ class Rectangle: public Shape {
 
 int main(void) {
 	Rectangle Rect;
+	int area;
 
 	Rect.setWidth(5);
 	Rect.setHeight(7);
 
+	area = Rect.getArea();
+
 	// Print the area of the object.
 	cout << "Total area: " << Rect.getArea() << endl;
+
+	// Print the total cost of painting
+	cout << "Totla paint cost: $" << Rect.getCost(area) << endl;
 
 	return 0;
 }
