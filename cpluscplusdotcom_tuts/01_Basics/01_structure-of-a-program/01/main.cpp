500cab9dac4ff42dcc6914b0644707d89ddfced7
hopo <hpsight@gmail.com>
2017-12-05 00:30:31 +0900

    cppdcom ~

---
+++ cpluscplusdotcom_tuts/01_Basics/01_structure-of-a-program/01/main.cpp
@@ -0,0 +1,7 @@
+// my first program in c++
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+}
---
+++ cpluscplusdotcom_tuts/01_Basics/01_structure-of-a-program/02/main.cpp
@@ -0,0 +1,8 @@
+// my second program in C++
+#include <iostream>
+
+int main ()
+{
+	std::cout << "Hello Wordl! ";
+	std::cout << "I'm a C++ program";
+}
