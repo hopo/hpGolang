7f236cc885e82a5ec5f41f85b96f59879f30ccdf
hopo <hpsight@gmail.com>
2017-12-09 13:17:44 +0900

    ~

--- CheckIO/01_Elementary/02_correct-sentence/cpp-01/main.cpp
+++ CheckIO/01_Elementary/02_correct-sentence/cpp-01/main.cpp
@@ -1,32 +1,11 @@
-// ...ing
 #include <iostream>
-#include <string>
-#include <ctype.h>
-#include <stdio.h>
-
-using namespace std;
-
-string correct_sentence(string text) { 
-	string ret = text;
-	if (islower(ret[0])) {
-		ret[0] = ret[0]-32; 
-	}
-	
-	int l = sizeof(ret);
-
-	cout << l;
-    return "\n***";
-}
+#include <vector>
 
 int main() {
-    //string ex1 = correct_sentence("greetings, friends"); // "Greetings, friends."
-    //string ex2 = correct_sentence("Greetings, friends"); // "Greetings, friends."
-    string ex3 = correct_sentence("Greetings, friends."); // "Greetings, friends."
-    //string ex4 = correct_sentence("hi"); // "Hi."
+	srd::vector<int> abc;
 
-	//cout << ex1 << endl;
-	//cout << ex2 << endl;
-	cout << ex3 ;
-	//cout << ex4 << endl;
+	cout << abc << '\n';
+
+	return 0;
 }
 
