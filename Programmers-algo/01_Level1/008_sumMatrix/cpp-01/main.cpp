b4e3fc9ae3d03fc9bdca94145b8d1b80c42b7116
hopo <hpsight@gmail.com>
2017-12-09 18:08:44 +0900

    ~

--- Programmers-algo/01_Level1/008_sumMatrix/cpp-01/main.cpp
+++ Programmers-algo/01_Level1/008_sumMatrix/cpp-01/main.cpp
@@ -1,26 +1,24 @@
 // https://programmers.co.kr/learn/challenge_codes/148
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-vector<vector<int> > sumMatrix(vector<vector<int> >A, vector<vector<int> >B)
-{
-	vector<vector<int> > answer;
+vector<vector<int>> sumMatrix(vector<vector<int>>A, vector<vector<int>>B) {
+	vector<vector<int>> answer;
 
 	return answer;
 }
-int main()
-{
-	vector<vector<int> > a{{1,2},{2,3}}, b{{3,4},{5,6}};
-	vector<vector<int> > answer = sumMatrix(a,b);
 
-	for(int i=0;i<answer.size();i++)
-	{
-		for(int j=0;j<answer[0].size();j++)
-		{
-			cout<<answer[i][j]<<" ";
+int main() {
+	vector<vector<int>> a{{1,2},{2,3}}, b{{3,4},{5,6}}; // {{4, 6}, {{7, 9}}
+	vector<vector<int>> answer = sumMatrix(a,b);
+
+	for(int i = 0; i < answer.size(); i++) {
+		for(int j = 0; j < answer[0].size(); j++) {
+			cout << answer[i][j] << " ";
 		}
-		cout<<"\n";
+		cout << "\n";
 	}
 }
