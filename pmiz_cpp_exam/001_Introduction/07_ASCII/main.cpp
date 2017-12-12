b975566482f60792344b22efad36fc39d68abf0c
hopo <hpsight@gmail.com>
2017-12-12 17:11:38 +0900

    pmiz Introduction

---
+++ pmiz_cpp_exam/001_Introduction/07_ASCII/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char c;
+	cout << "Enter a chracter: ";
+	cin >> c;
+	cout << "ASCII Value of " << c << " is " << int(c);
+
+	return 0;
+}
