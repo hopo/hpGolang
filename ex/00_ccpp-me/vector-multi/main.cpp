69876b67a1c6785b29f9d1c7c6b086108e57ce8c
hopo <hpsight@gmail.com>
2017-12-12 21:38:08 +0900

    ex_ccpp map

--- ex/00_ccpp-me/vector-multi/main.cpp
+++ ex/00_ccpp-me/vector-multi/main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-	vector<vector<int>>::iterator mmm;
+	vector<vector<int>>::iterator vtm;
 
 	int m;
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
 			m = i * j;
-			mmm[i][j] = m;
-			cout << "i" << i << ": " << mmm[i][j] << ", ";
+			vtm[i][j] = m;
+			cout << "i" << i << ": " << vtm[i][j] << "\n";
 		}
 	}
 
