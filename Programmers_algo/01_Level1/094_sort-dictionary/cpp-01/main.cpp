21a6bfe0b9df6cb52d02cce214defff9f1f1ffa2
hopo <hpsight@gmail.com>
2017-12-14 00:25:18 +0900

    ~

--- Programmers_algo/01_Level1/094_sort-dictionary/cpp-01/main.cpp
+++ Programmers_algo/01_Level1/094_sort-dictionary/cpp-01/main.cpp
@@ -4,12 +4,15 @@
 #include <vector>
 #include <map>
 
-struct Ncard {
-	name   string
-	number int
-}
+using namespace std;
 
-void sort_dictionary(map[string]int) {
+void sort_dictionary(map<string, int> ncard) {
+	for(auto &x : ncard) { // &  what???
+		cout << x << " - ";
+		cout << x.first << " : " << x.second << endl;
+	}
+	
+ 	/*
 	vector<string> 
 	for k, _ := range nc {
 		slis = append(slis, k)
@@ -19,9 +22,10 @@ void sort_dictionary(map[string]int) {
 		ret = append(ret, Ncard{k, nc[k]})
 	}
 	return
+	*/
 }
 
 int main() {
-	map<string, int> ncard {"김철수": 78, "이하나": 97, "정진원": 88}
-	ort_dictionary(ncard);
+	map<string, int> ncard = {{"김철수", 78}, {"이하나", 97}, {"정진원", 88}};
+	sort_dictionary(ncard);
 }
