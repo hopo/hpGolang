9dc1e05c2d5d89da649c672eaaab972f8664818d
hopo <hpsight@gmail.com>
2017-12-29 19:22:53 +0900

    tPoint_c union

---
+++ tPoint_c/15_BitFields/02_bit-field-declaration/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+struct
+{
+	unsigned int age : 3;
+} Age;
+
+int main() {
+	Age.age = 4;
+	printf("sizeof(Age) : %d\n", sizeof(Age));
+	printf("Age.age : %d\n", Age.age);
+
+	Age.age = 7;
+	printf("Age.age : %d\n", Age.age);
+
+	Age.age = 8;
+	printf("Age.age : %d\n", Age.age);
+
+	return 0;
+}
