e2b1e817c41da51eeec05bfbda4ed756e307081e
hopo <hpsight@gmail.com>
2017-12-30 12:33:30 +0900

    tPoint_c

---
+++ tPoint_c/25_CommandLineArguments/02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("Program name %s\n", argv[0]);
+
+	if(argc == 2) {
+		printf("The argument supplied is %s\n", argv[1]);
+	}
+	else if(argc > 2) {
+		printf("Too many arguments supplied.\n");
+	}
+	else {
+		printf("One aregument expected.\n");
+	}
+}
