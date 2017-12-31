1f316ac4f5661ff36c25bda889c6d5c499308462
hopo <hpsight@gmail.com>
2017-12-31 22:58:23 +0900

    ~

--- hinclude/chstd.h
+++ hinclude/chstd.h
@@ -1,10 +1,17 @@
 // ***********************
-// *** For c api by hp ***
+// *** For C api by hp ***
 // ***********************
 
 
 #ifndef CHSTD_H
 
+// methods
+int carrlen(char *);
+
+
+// carrlen()
+// p: char *
+// r: int
 int carrlen(char *arr) {
 	int length = 0;
 	while(1) {
