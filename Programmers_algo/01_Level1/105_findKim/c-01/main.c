f0b5e5731d0119fbaaa1e686f911cf465ca5cb24
hopo <hpsight@gmail.com>
2018-01-09 15:43:11 +0900

    Palgo Level1

---
+++ Programmers_algo/01_Level1/105_findKim/c-01/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int findKim(char *names[]);
+
+int main() {
+	char *names1[] = {"Queen", "Tod", "Kim"};	// 2
+	int ex1 = findKim(names1);
+	printf("%d", ex1);
+
+	char *names2[] = {"Gim", "Obama", "xi"};	// -1
+	int ex2 = findKim(names2);
+	printf("\n%d", ex2);
+
+	return 0;
+}
+
+int findKim(char *names[]) {
+	int lnth = 3;
+	for(int i = 0; i < lnth; ++i) {
+		if(names[i] == "Kim") { return i; }
+	}
+	return -1;
+}
