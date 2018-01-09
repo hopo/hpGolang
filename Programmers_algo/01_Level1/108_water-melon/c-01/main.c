0be2b6ef70c1bf3e544f6d63200a64a3abfb06d7
hopo <hpsight@gmail.com>
2018-01-09 16:43:05 +0900

    Palgo Level1

---
+++ Programmers_algo/01_Level1/108_water-melon/c-01/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char* water_melon(int);
+
+int main() {
+	char* ex1 = water_melon(3);	// "WMW"
+	printf("%s", ex1);
+
+	char* ex2 = water_melon(4);	// "WMWM"
+	printf("\n%s", ex2);
+
+	return 0;
+}
+
+char* water_melon(int num) {
+	char ret[num+1];
+	int r = 0;
+	for(int i = 1; i < num+1; ++i) {
+		(i%2 == 1)? (ret[r] = 'W') : (ret[r] = 'M');
+		r++;
+	}
+	ret[r] = '\0';
+
+	char* dt = ret;
+
+	return dt;
+}
