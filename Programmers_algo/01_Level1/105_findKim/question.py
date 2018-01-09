da0ccd1af4675302c1af3d8ab38559c4b9a6f6dd
hopo <hpsight@gmail.com>
2018-01-09 19:34:43 +0900

    pmiz ArrayAndPointer

---
+++ Programmers_algo/01_Level1/105_findKim/question.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/challenge_codes/105
+
+def findKim(seoul):
+    kimIdx = 0
+    # 함수를 완성하세요
+
+    return "김서방은 {}에 있다".format(kimIdx)
+
+
+# 실행을 위한 테스트코드입니다.
+print(findKim(["Queen", "Tod", "Kim"]))
