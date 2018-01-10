81de07b2d3a2ae4efe971cb1ea905915fcebf177
hopo <hpsight@gmail.com>
2018-01-10 16:30:41 +0900

    pmiz Strings

---
+++ pmiz_exam/005_Strings/01_characters/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	char str[1000], ch;
+	int i, frequency = 0;
+
+	printf("Enter a string: ");
+	gets(str);
+
+	printf("Enter a character to find the frequency: ");
+	scanf("%c", &ch);
+
+	for(i = 0; str[i] != '\0'; ++i) {
+		if(ch == str[i]) { ++frequency; }
+	}
+
+	printf("Frequenct of %c = %d", ch, frequency);
+
+	return 0;
+}
