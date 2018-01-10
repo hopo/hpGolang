fe61bbc9f2f89cf3cae34339cdd26f6100e64f35
hopo <hpsight@gmail.com>
2018-01-10 21:35:33 +0900

    ~

---
+++ ex/00_c-me/int-array/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void rm_small(int*);
+
+int main() {
+	int arr[] = {10, 8, 22};
+	rm_small(arr);
+	
+	// find length A or B
+	int size;
+	size = *(&arr+1) - arr; 
+	// size = sizeof(arr)/sizeof(arr[0]);
+	printf("\nlenth: %d", size);
+
+	return 0;
+}
+
+void rm_small(int* arr) {
+	printf("%d", arr[0]);
+	printf("\n%d", arr[1]);
+	printf("\n%d", arr[2]);
+	printf("\n%d", arr[3]);
+
+	
+}
+
