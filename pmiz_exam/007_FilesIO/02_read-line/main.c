cf182ffdc8bb9a8e0829f85e028d29ad86b0b88c
hopo <hpsight@gmail.com>
2018-01-12 13:15:51 +0900

    pmiz FilesIO

---
+++ pmiz_exam/007_FilesIO/02_read-line/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h> // For exit() function
+
+int main() {
+	char c[1000];
+	FILE *fptr;
+
+	if((fptr = fopen("program.txt", "r")) == NULL) {
+		printf("Error! opening file");
+		// Program exits if file pointer returns NULL.
+		exit(1);
+	}
+
+	// reads text until newline
+	fscanf(fptr, "%[^\n]", c);
+
+	printf("Data from the file: %s", c);
+
+	return 0;
+}
