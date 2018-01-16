4419098ef569bec6b8d1c2929708005ded82154a
hopo <hpsight@gmail.com>
2018-01-16 19:53:12 +0900

    ~

---
+++ ex/01_c-other/next_cArch-bits-ex/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int bitxor(int, int);	// 두변수의 XOR 계산
+int allEvenBits(int, int);	// 짝수번째비트가'1'인가?
+int getByte(int, int);	//	변수x, n번째의 byte?
+int minusOne(int, int);	// '-1' return.
+int fitsBits(int, int);	// 변수x가n비트로표현가능한가?
+int addOK(int, int);	// is it overflow??
+
+int main() {
+	return 0;
+}
