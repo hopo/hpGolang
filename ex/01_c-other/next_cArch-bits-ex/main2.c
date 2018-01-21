3a0929772020aa641b70b5a60252bec591d61340
hopo <hpsight@gmail.com>
2018-01-21 22:28:57 +0900

    ~

--- ex/01_c-other/next_cArch-bits-ex/main2.c
+++ ex/01_c-other/next_cArch-bits-ex/main2.c
@@ -12,7 +12,7 @@
 */
 
 void bitxor(int, int);	// xor operator of two int
-void allEvenBits(int, int);	// 짝수번째비트가'1'인가?
+void allEvenBits(int);	// 짝수번째비트가'1'인가?
 void getByte(int, int);	//	변수x, n번째의 byte?
 void minusOne(int, int);	// '-1' return.
 void fitsBits(int, int);	// 변수x가n비트로표현가능한가?
@@ -21,12 +21,20 @@ void addOK(int, int);	// is it overflow??
 int main() {
 	int a = 0x04;
 	int b = 0x03;
-	bitxor(a, b);
+
+	// bitxor(a, b);
+	allEvenBits(a);
 
 	return 0;
 }
 
+// xor operator of two int
 void bitxor(int a, int b) {
 	printf("%d", (a|b)+(~(a&b)+1));	// using complement
 	printf(" %d ", a^b);	// ^ operator
+}
+
+void allEvenBits(int a) {
+	int x;
+	printf("%#x", x);
 }
