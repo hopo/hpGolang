f68e80bf3c2e79502c4d06600f11996bb4fdcae7
hopo <hpsight@gmail.com>
2018-01-29 23:06:59 +0900

    ~

--- ex/10_asm-other/helloworld.s
+++ ex/10_asm-other/helloworld.s
@@ -5,12 +5,12 @@ section .text
 	global _start
 
 _start:
-	mov eax, 1
-	mov edi, 1
-	mov esi, msg
-	mov edx, 15
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 15
 	syscall
 
-	mov eax, 60
-	mov edi, 0
+	mov rax, 60
+	mov rdi, 0
 	syscall
