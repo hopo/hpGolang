b6a1cbfd412001a5b9f29882f5bd9d1084fd9449
hopo <hpsight@gmail.com>
2018-02-02 18:25:47 +0900

    Palgo Level2

--- Programmers_algo/02_Level2/088_is_pair/c-01/main.c
+++ Programmers_algo/02_Level2/088_is_pair/c-01/main.c
@@ -4,47 +4,60 @@ int is_pair(char *);
 
 int  main() {
 	char *str1;
-	str1 = "(hello)()";	// 1, true
+	str1 = "(hello)()";
 	int ex1;
-	ex1 = is_pair(str1);
+	ex1 = is_pair(str1);	/* 1, true */
+	printf("%d\n", ex1);
 
-	/*
-	ex2 := is_pair(")(")                       // false
-	ex3 := is_pair("(5+(40*(1+2)/2)+(1+2)*2)") // true
-	fmt.Println(ex1)
-	fmt.Println(ex2)
-	fmt.Println(ex3)
-	*/
+	char *str2;
+	str2 = ")(";
+	int ex2;
+	ex2 = is_pair(str2);	/* 0, false */
+	printf("%d\n", ex2);
+
+	char *str3;
+	str3 = "(5+(40*(1+2)/2)+(1+2)*2)";
+	int ex3;
+	ex3 = is_pair(str3);	/* 1, true */
+	printf("%d", ex3);
 }
 
 int is_pair(char *str) {
-	printf("%s", str);
+	int i = 0, b = 0;
+	char box[32];
+
+	// how many brackets
+	while(str[i] != '\0') {
+		if(str[i] == '(' || str[i] == ')') {
+			box[b] = str[i];
+			b++;
+
+			if(b > 31) {	/* if box length over */
+				printf("* TOO MANY BRACKETS * ");
+				return -1;
+			}
+		}
+		i++;
+	}
+	box[b] = '\0';
 	
-	return -1;
-
-	/*
-	bsl := []byte(s)
-	var smpl []byte
-	for _, v := range bsl {
-		if v == 40 || v == 41 {
-			smpl = append(smpl, v)
-		}
-	}
-	l := len(smpl)
-	var chk int
-	for _, v := range smpl {
-		if v == 40 {
-			chk++
-		}
-	}
-	a := l%2 == 0
-	b := smpl[0] == 40
-	c := smpl[l-1] == 41
-	d := chk == l/2
-	if a && b && c && d {
-		return true
+	// how many '('
+	int chk = 0;
+	i = 0;
+	while(box[i] != '\0') {
+		if(box[i] == '(') { chk++; }
+		i++;
 	}
 
-	return false
-	*/
+	// 4 flag 
+	int w, x, y, z;
+	w = (b%2 == 0)? 1 : 0; 
+	x = (box[0] == '(')? 1 : 0;
+	y = (box[b-1] == ')')? 1 : 0;
+	z = (b/2 == chk)? 1 : 0;
+
+	int ret;
+	ret = (w&x&y&z)? 1 : 0;
+	
+	return ret;
 }
