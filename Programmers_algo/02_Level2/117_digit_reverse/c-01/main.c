aa6869db12114c646b3c75fa8e95af4babcb8528
hopo <hpsight@gmail.com>
2018-02-17 02:09:47 +0900

    Palgo level 2

--- Programmers_algo/02_Level2/117_digit_reverse/c-01/main.c
+++ Programmers_algo/02_Level2/117_digit_reverse/c-01/main.c
@@ -1,4 +1,3 @@
-// ing
 #include <stdio.h>
 
 void digit_reverse(int num, int *ret);
@@ -6,53 +5,32 @@ void digit_reverse(int num, int *ret);
 int main() {
 	int num1 = 12345, ret1[16];
 	digit_reverse(num1, ret1); // [5 4 3 2 1]
-	printf("[%d %d %d %d %d]", ret1[0], ret1[1], ret1[2], ret1[3], ret1[4]);
+	printf("[%d %d %d %d %d]\n", ret1[0], ret1[1], ret1[2], ret1[3], ret1[4]);
 
-	/*
 	int num2 = 321, ret2[16];
 	digit_reverse(num2, ret2);   // [1 2 3]
-	*/
+	printf("[%d %d %d]", ret2[0], ret2[1], ret2[2]);
 
 	return 0;
 }
 
-void digit_reverse(int num, int*ret) {
-	
-}
-
-/*
-
-func digit_reverse(n int) []int {
-	isl := int_convert_each(n)
-	var ret []int
-	for i := 0; i < len(isl); i++ {
-		j := len(isl) - 1 - i
-		ret = append(ret, isl[j])
-	}
-	return ret
-
-}
-
-func int_convert_each(num int) []int {
-	numlen := 10
-	for {
-		if num-numlen < 0 {
-			numlen /= 10
-			break
+void digit_reverse(int num, int *ret) {
+	int ea, nl = 10, lnth = 1;
+	while(1) {
+		if(nl-num > 0) {
+			nl /= 10;
+			break;
 		}
-		numlen *= 10
+		nl *= 10;
+		lnth++;
 	}
-	var ret []int
-	var ea int
-	for {
-		ea = num / numlen
-		ret = append(ret, ea)
-		num %= numlen
-		numlen /= 10
-		if numlen < 1 {
-			break
-		}
+
+	while(1) {
+		ea = num / nl;
+		ret[lnth-1] = ea;
+		lnth--;
+		num %= nl;
+		nl /= 10;
+		if(nl < 1) { break; }
 	}
-	return ret
 }
-*/
