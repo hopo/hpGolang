f4ede614a5bb68762cc8dceac4e4fea8ed1afa0a
hopo <hpsight@gmail.com>
2018-03-07 02:56:22 +0900

    ~

--- tPoint_java/src/a01_BasicSyntax/MyFirstJavaProgram.java
+++ tPoint_java/src/a01_BasicSyntax/MyFirstJavaProgram.java
@@ -7,6 +7,6 @@ public class MyFirstJavaProgram {
     */
 
    public static void main(String args[]) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World"); // prints "Hello World"
    }
 }
