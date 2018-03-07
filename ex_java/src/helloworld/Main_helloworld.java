62804062777eac6c9761ed7aeef0c02ed8f46c03
hopo <hpsight@gmail.com>
2018-03-08 02:36:22 +0900

    ex_java

---
+++ ex_java/src/helloworld/Main_helloworld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class Main_helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Hello Work!");
+	}
+}
---
+++ ex_java/src/statictest/Main_statictest.java
@@ -0,0 +1,12 @@
+package statictest;
+
+public class Main_statictest {
+	public static void main(String[] args) {
+		String str = "static~!";
+		prnt(str);
+	}
+
+	public static void prnt(String str) {
+		System.out.print(str);
+	}
+}
