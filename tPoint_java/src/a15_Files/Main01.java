809b77d7d030bb465090cc5baf6735318f6ac63c
hopo <hpsight@gmail.com>
2018-03-11 03:03:00 +0900

    .

---
+++ tPoint_java/src/a15_Files/Main01.java
@@ -0,0 +1,17 @@
+package a15_Files;
+
+import java.io.File;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		File file1 = new File("./demo1.txt");
+		File file2 = new File("./demo2.txt");
+		
+		if(file1.compareTo(file2) == 0) {
+			System.out.println("Both path are same!");
+		} else {
+			System.out.println("Path are not same!");
+		}
+	}
+}
