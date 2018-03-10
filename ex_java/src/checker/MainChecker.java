f6ee3a9d65204984098741859e09bd7ca94d8342
hopo <hpsight@gmail.com>
2018-03-10 21:13:00 +0900

    .

---
+++ ex_java/src/checker/MainChecker.java
@@ -0,0 +1,19 @@
+package checker;
+
+public class MainChecker {
+  public static void main(String args[]) {
+    String str = new String("a");
+
+    switch(str) {
+      case "a":
+        System.out.println("this is "+str);
+        break;
+      case "b":
+        System.out.println("this is "+str);
+        break;
+      case "c":
+        System.out.println("this is "+str);
+        break;
+    }
+  }
+}
---
+++ ex_java/src/whiletest/MainWhile.java
@@ -0,0 +1,12 @@
+package whiletest;
+
+public class MainWhile {
+  public static void main(String args[]) {
+    int i = 0; 
+    while(i != 10) {
+       System.out.print("index:"); 
+       System.out.println(i); 
+       i++;
+    }
+  }
+}
