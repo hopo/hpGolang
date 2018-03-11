6c1aa771cd85b25c387f0b9305328cb2a7eb76fe
hopo <hpsight@gmail.com>
2018-03-11 22:56:10 +0900

    .

---
+++ ex_java/src/sum/MainSum.java
@@ -0,0 +1,11 @@
+package sum;
+
+public class MainSum {
+  public static void main(String args[]) {
+    int a = 10, b = 20;
+    int sum = (a+b);
+    sum += sum;
+
+    System.out.print(sum);
+  }
+}
