54d367e08827bee6aa450e14cd0c9273c50882fa
hopo <hpsight@gmail.com>
2018-03-12 15:21:39 +0900

    ~

---
+++ tPoint_java/src/a16_Exceptions/ExcepTest.java
@@ -0,0 +1,18 @@
+package a16_Exceptions;
+
+// File Name : ExceptTest.java
+import java.io.*;
+
+public class ExcepTest {
+	
+	public static void main(String[] args) {
+		try {
+			int a[] = new int[2];
+			System.out.println("Access element three :" + a[3]);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Exception thrown :" + e);
+		}
+		System.out.println("Out of the block");
+	}
+
+}
---
+++ tPoint_java/src/a16_Exceptions/Unchecked_Demo.java
@@ -0,0 +1,9 @@
+package a16_Exceptions;
+
+public class Unchecked_Demo {
+	public static void main(String[] args) {
+		int num[] = {1, 2, 3, 4};
+		System.out.println(num[5]);
+	}
+
+}
