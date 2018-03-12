f12a5ddc0e47420be918aa19e0cb786754894f1c
hopo <hpsight@gmail.com>
2018-03-12 21:41:47 +0900

    z

---
+++ jtPoint_java/src/a17_InnerClasses/My_class02.java
@@ -0,0 +1,26 @@
+package a17_InnerClasses;
+
+class Outer_Demo02{
+    // private variable of the outer class
+    private int num = 175;
+
+    // inner class
+    public class Inner_Demo02 {
+        public int getNum() {
+            System.out.print("This is the getnum method of the inner class ");
+            return num;
+        }
+    }
+}
+
+public class My_class02 {
+
+    public static void main(String args[]) {
+        // Instantiating the outer class
+        Outer_Demo02 outer = new Outer_Demo02();
+
+        // Instantiating the inner class
+        Outer_Demo02.Inner_Demo02 inner = outer.new Inner_Demo02();
+        System.out.println(inner.getNum());
+    }
+}
---
+++ jtPoint_java/src/a17_InnerClasses/My_class03.java
@@ -0,0 +1,25 @@
+package a17_InnerClasses;
+
+// interface
+interface  Message {
+    String greet();
+}
+
+public class My_class03 {
+    // method which accepts the object of interface Message
+    public void displayMessage(Message m) {
+        System.out.println(m.greet() + ", This is an example of anonymous inner class as an argumet");<`0`>
+    }
+
+    public static void main(String args[]) {
+        // Instantiating the class
+        My_class03 obj = new My_class03();
+
+        // Passing an anonymous inner class as an argument
+        obj.displayMessage(new Message() {
+            public String greet() {
+                return "Hello";
+            }
+        });
+    }
+}
