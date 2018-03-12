d1b8dc19642591b14fba2c6bac60a9ff2962174d
hopo <hpsight@gmail.com>
2018-03-12 23:15:29 +0900

    ...

---
+++ jPalgo/src/level01/GcdLcm.java
@@ -0,0 +1,80 @@
+package level01;
+
+import java.util.Arrays;
+
+public class GcdLcm {
+	public static void main(String[] args) {
+		GcdLcm gl1 = new GcdLcm();
+		int[] ex1 = gl1.gcdlcm(3, 12);	// 3, 12
+		System.out.println(Arrays.toString(ex1));
+
+		GcdLcm gl2 = new GcdLcm();
+		int[] ex2 = gl2.gcdlcm(4, 7);	// 1, 28
+		System.out.println(Arrays.toString(ex2));
+	}
+
+	int[] gcdlcm(int a, int b) {
+		int tmp;
+		if(a > b) {
+			tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+		int mn = a, mx = b;
+		while(mn > 0) {
+			tmp = mx%mn;
+			mx = mn;
+			mn = tmp;
+		}
+
+		int[] ret = {mx, (int)((a*b)/mx)};
+		return ret;
+	}
+}
+
+
+/*
+int gcd(int n1, int n2);
+int lcm(int n1, int n2);
+int* gcdlcm(int n1, int n2);
+
+
+int gcd(int n1, int n2) {
+	// bigger - smaller ?
+	while(n1 != n2) {
+		if(n1 > n2) { n1 -= n2; }
+		else { n2 -= n1; }
+	} 
+
+	return n1;
+}
+
+int lcm(int n1, int n2) {
+	int max;
+
+	// maximum value between n1 and n2 is stored in max
+	max = (n1 > n2)? n1 : n2;
+
+	do {
+		if(max%n1 == 0 && max%n2 == 0) { break; }
+		else { ++max; }
+	}
+	while(1);
+
+	return max;
+}
+
+int* gcdlcm(int n1, int n2) {
+	int r1, r2;
+	r1 = gcd(n1, n2);
+	r2 = lcm(n1, n2);
+
+	int ret[] = {r1, r2};
+
+	int* dt = ret;
+
+	return dt;
+}
+
+*/
---
+++ jPalgo/src/level01/StringMiddle.java
@@ -0,0 +1,33 @@
+package level01;
+
+public class StringMiddle {
+	public static void main(String[] args) {
+		StringMiddle sm1 = new StringMiddle();
+		String str1 = new String("power");
+		String ex1 = sm1.stringmiddle(str1); // w
+		System.out.println(ex1);
+
+/*
+		StringMiddle sm2 = new StringMiddle();
+		String str2 = new String("java");
+		String ex2 = sm2.stringmiddle(str2); // av
+		System.out.print(ex2);
+*/
+	}
+
+	String stringmiddle(String str) {
+		int str_len = str.length();
+		int str_len_half = str_len/2;
+		/*
+		if(str_len%2 == 0) {
+			return str[str_len_half-1] + len[str_len_half+1];
+		}
+		*/
+		return "break! String";
+	}
+}
+
+
+/*
+return len(str)&1 and str[len(str)//2] or str[len(str)//2-1:len(str)//2+1]
+*/
