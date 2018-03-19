dbee71c4e9398d396c89034e3f143ed9cb7b3f7c
hopo <hpsight@gmail.com>
2018-03-19 14:42:58 +0900

    Collections

---
+++ jtPoint_java/src/c01_Collections/AlgorithmsDemo.java
@@ -0,0 +1,46 @@
+package c01_Collections;
+
+// import java.util.Collections;
+// import java.util.Comparators;
+// import java.util.Iterator;
+// import java.util.LinkedList;
+import java.util.*;
+
+public class AlgorithmsDemo {
+    public static void main(String args[]) {
+        // Create and initialize linked list
+        LinkedList ll = new LinkedList();
+        ll.add(new Integer(-8));
+        ll.add(new Integer(20));
+        ll.add(new Integer(-20));
+        ll.add(new Integer(8));
+
+        // Create a reverse order comparator
+        Comparator r = Collections.reverseOrder();
+
+        // Sort list by using the comparato
+        Collections.sort(ll, r);
+
+        // Get iterator
+        Iterator li = ll.iterator();
+        System.out.print("List soreted in reverse: ");
+
+        while(li.hasNext()) {
+            System.out.print(li.next() + " ");
+        }
+        System.out.println();
+        Collections.shuffle(ll);
+
+        // display randomized list
+        li = ll.iterator();
+        System.out.print("List shffled: ");
+
+        while(li.hasNext()) {
+            System.out.print(li.next() + " ");
+        }
+        
+        System.out.println();
+        System.out.println("Minimum: " + Collections.min(ll));
+        System.out.println("Maximum: " + Collections.max(ll));
+    }
+}
---
+++ jtPoint_java/src/c01_Collections/LinkedListDemo.java
@@ -0,0 +1,36 @@
+package c01_Collections;
+
+import java.util.*;
+
+public class LinkedListDemo {
+    public static void main(String args[]) {
+        // create a linked list
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // add elemetnts to the linked list
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+        ll.addLast("Z");
+        ll.addFirst("A");
+        ll.add(1, "A2");
+        System.out.println("Originam contents of ll: " + ll);
+
+        // remove elements from the linked list
+        ll.remove("F");
+        ll.remove(2);
+        System.out.println("Contents of ll after deletion: " + ll);
+
+        // remove first and last elements 
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println("ll of deleting first and last: " + ll);
+
+        // get and set a value
+        Object val = ll.get(2);
+        ll.set(2, (String) val + " Changed");
+        System.out.println("ll after change: " + ll);
+    }
+}
