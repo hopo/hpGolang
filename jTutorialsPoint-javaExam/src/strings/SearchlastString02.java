e8c2167676529f13e9cdd36cc050ec39885e1ec9
hopo <hpsight@gmail.com>
2018-03-20 11:19:31 +0900

    ~

---
+++ jTutorialsPoint-javaExam/src/strings/SearchlastString02.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class SearchlastString02 {
+    public static void main(String args[]) {
+        String t1 =  "tutorialspoint";
+        int index = t1.lastIndexOf("p");
+        System.out.println(index);
+    }
+}
