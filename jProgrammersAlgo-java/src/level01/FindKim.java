94d4189f8ea012748274ac3e0d725859a23e81a9
hopo <hpsight@gmail.com>
2018-03-22 10:02:33 +0900

    ~

--- jProgrammersAlgo-java/src/level01/FindKim.java
+++ jProgrammersAlgo-java/src/level01/FindKim.java
@@ -1,23 +1,21 @@
-//ing
 package level01;
 
+public class FindKim {
+    public static void main(String args[]) {
+        String[] fnames1 = {"Queen", "Tod", "Kim"};
+        int ex1 = findkim(fnames1); //2
+        System.out.println(ex1);
 
+        String[] fnames2 = {"Gim", "Obama", "xi"};
+        int ex2 = findkim(fnames2); //-1
+        System.out.println(ex2);
+    }
 
-/*
-func main() {
-	ex1 := findKim([]string{"Queen", "Tod", "Kim"})
-	ex2 := findKim([]string{"Gim", "Obama", "xi"})
-	fmt.Println(ex1)
-	fmt.Println(ex2)
+    static int findkim(String[]  fnames) {
+        int ret = -1;
+        for(int i = 0; i < fnames.length; i++){
+            if(fnames[i] == "Kim"){ ret = i; }
+        }
+        return ret;
+    }    
 }
-
-func findKim(fnames []string) (ret int) {
-	ret = -1
-	for i, v := range fnames {
-		if v == "Kim" {
-			ret = i
-		}
-	}
-	return
-}
-*/
