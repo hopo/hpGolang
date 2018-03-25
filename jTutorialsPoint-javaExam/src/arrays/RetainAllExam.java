9908f801009e7bb11c8c295075f24ca42c829fce
hopo <hpsight@gmail.com>
2018-03-25 17:28:10 +0900

    -

--- jTutorialsPoint-javaExam/src/arrays/RetainAllExam.java
+++ jTutorialsPoint-javaExam/src/arrays/RetainAllExam.java
@@ -2,25 +2,25 @@ package arrays;
 
 import java.util.ArrayList;
 
-public class RemoveAll {
+public class RetainAllExam {
     public static void main(String args[]) {
         ArrayList<String> objArray = new ArrayList<>();
-        ArrayList<String> objArray2 = new ArrayList<>();
+        ArrayList<String>  objArray2 = new ArrayList<>();
 
         objArray.add(0, "common1");
         objArray.add(1, "common2");
         objArray.add(2, "notcommon2");
-
+        
         objArray2.add(0, "common1");
         objArray2.add(1, "common2");
         objArray2.add(2, "notcommon");
         objArray2.add(3, "notcommon1");
 
-        System.out.println("Array elements of array1: " + objArray);
-        System.out.println("Array elements of array2: " + objArray2);
+        System.out.println("Array elements of array1" + objArray);
+        System.out.println("Array elements of array2" + objArray2);
 
-        // objArray - objArray2
-        objArray.removeAll(objArray2);
-        System.out.println("Array after removing array2 from array1: " + objArray);
+        // objArray ^ objArray2, inter
+        objArray.retainAll(objArray2);
+        System.out.println("Array1 after retaining common elements of array2 & array1" + objArray);
     }
 }
