1ecd09cd4780a6bb0104e515a0a1b56b94008d6f
hopo <hpsight@gmail.com>
2018-03-30 19:28:43 +0900

    .

--- jEx/src/arraytest/Extending.java
+++ jEx/src/arraytest/Extending.java
@@ -1,27 +1,25 @@
-import java.util.Arrays;
-
 // package arraytest;
+
+import java.util.Arrays;
 
 public class Extending {
     public static void main(String args[]) {
-        Alist alist = new Alist();
-        for(int i = 0; i < 10; i++) {
-            alist.add(i);
+        Arrlist list = new Arrlist();
+        int num = 10;
+        for(int i = 0; i < num; i++) {
+            list.add(i);
         }
 
-        // printing
-        for(int i = 0; i < alist.length; i++) {
-            System.out.print(alist[i] + " ");
-        }
     }
 }
 
-class Alist {
+class Arrlist {
     private int size = 0;
     private Object[] oarr = new Object[10];
     
     public boolean add(Object e) {
-        oarr[size++] = e;
+        oarr[size] = e;
+        size++;
         return true;
     }
 }
