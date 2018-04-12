1ef562eefc7f34c438624adb0c624b2479c6a573
hopo <hpsight@gmail.com>
2018-04-12 09:12:38 +0900

    ~

--- NextIT-pc33/lec-DB/start-sqldev.sql
+++
@@ -1,8 +0,0 @@
-create tablespace study datafile '/oracdata/sudy.dbf'
-size 100M autoextend on next 10M;
-
-create user java identified by oracle
-default tablespace study;
-
-grant connect to java;
-grant resource to java;
