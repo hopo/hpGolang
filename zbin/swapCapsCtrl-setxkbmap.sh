0e712ab01f0451b6beed114c3a8d73c1411d1c2e
hopo <hpsight@gmail.com>
2018-04-19 17:48:32 +0900

    -

---
+++ zbin/swapCapsCtrl-setxkbmap.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+# caps <-> ctrl
+setxkbmap -option "ctrl:swapcaps"
+
+# caps <-> esc
+# setxkbmap -option "caps:swapescape"
+
+# alt <-> win
+# setxkbmap -option altwin:swap_alt_win
+
