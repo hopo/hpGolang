687cfd6bdd000f33d4256f64bc43a89a4e2e1ba2
hopo <hpsight@gmail.com>
2018-04-22 00:44:31 +0900

    -

--- ProgrammersAlgo/01_Level1/002_sumDivisor/py-01/main.py
+++ ProgrammersAlgo/01_Level1/002_sumDivisor/py-01/main.py
@@ -3,5 +3,9 @@ def sumDivisor(num):
     return sum([0 if num%n else n for n in range(1, num+1)])
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(sumDivisor(12))
+if __name__ == '__main__':
+    print(sumDivisor(12)) # 28, 1+2+3+4+6+12
+
+    print(sumDivisor(4)) # 7, 1+2+4
+    
 
