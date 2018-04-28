ee155631cd18e9662f3acb1104afd769b27cb20b
hopo <hpsight@gmail.com>
2018-04-28 23:01:59 +0900

    -

--- ProgrammersAlgo/02_Level2/020_numOfPrime/py-01/main.py
+++ ProgrammersAlgo/02_Level2/020_numOfPrime/py-01/main.py
@@ -2,14 +2,19 @@
 
 def num_of_prime(num):
     
+    for n in range(num):
+        print(n)
+    
     return -1
 
 if __name__ == '__main__':
     ex1 = num_of_prime(10) # 4, [2 3 5 7]
     print(ex1)
 
+    """
     ex2 = num_of_prime(5) # 3, [2 3 5]
     print(ex2)
+    """
 
 """
 func numOfPrime(n int) int {
