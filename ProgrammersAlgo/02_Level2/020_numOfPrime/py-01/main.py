5550b50bdd1e8b41a67d89dab7a48011be96e55a
hopo <hpsight@gmail.com>
2018-05-07 13:33:58 +0900

    -

--- ProgrammersAlgo/02_Level2/020_numOfPrime/py-01/main.py
+++ ProgrammersAlgo/02_Level2/020_numOfPrime/py-01/main.py
@@ -1,39 +1,28 @@
-# ...ing
-
 def num_of_prime(num):
-    
-    for n in range(num):
-        print(n)
-    
-    return -1
+    pm_count = 0
+
+    for n in range(2, num+1):
+        if is_prime(n):
+            pm_count += 1
+
+    return pm_count
+
+
+def is_prime(num):
+    isp = True; 
+
+    for n in range(2, num//2+1):
+        if num%n == 0:
+            isp = False
+            break
+
+    return isp
+
 
 if __name__ == '__main__':
     ex1 = num_of_prime(10) # 4, [2 3 5 7]
     print(ex1)
 
-    """
     ex2 = num_of_prime(5) # 3, [2 3 5]
     print(ex2)
-    """
 
-"""
-func numOfPrime(n int) int {
-	r := makenop(n)
-	return len(r)
-}
-
-func makenop(lmt int) []int {
-	var isl []int
-	for i := 2; i < lmt+1; i++ {
-		isl = func(isl []int, i int) []int {
-			for _, v := range isl {
-				if i%v == 0 {
-					return isl
-				}
-			}
-			return append(isl, i)
-		}(isl, i)
-	}
-	return isl
-}
-"""
