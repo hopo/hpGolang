50aec5df82f1c412fabc3c8013692a2e77bf3bfc
hopo <hpsight@gmail.com>
2018-05-09 23:20:03 +0900

    PAlgo lv2 py

--- ProgrammersAlgo/02_Level2/085_longest_palindrom/py-01/main.py
+++ ProgrammersAlgo/02_Level2/085_longest_palindrom/py-01/main.py
@@ -1,8 +1,15 @@
-# ing
 
 def longest_palindrom(s):
-    return 0
+    i = 0
+
+    for n in range(len(s)-1, -1, -1): #for n in reversed(range(len(s))):
+        if s[i] == s[n]:
+            i += 1
+        else:
+            i = 0
+    
+    return i
 
 if __name__ == '__main__':
-    print(longest_palindrom("토마토맛토마토"))
-    print(longest_palindrom("토마토맛있어"))
+    print(longest_palindrom("토마토맛토마토")) #7
+    print(longest_palindrom("토마토맛있어")) #3
