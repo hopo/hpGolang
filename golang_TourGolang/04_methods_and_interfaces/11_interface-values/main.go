66481757917898eb442df8afe3291a7ac15bc30b
HPMBA <hp@HPMBA.local>
2020-02-28 01:34:26 +0900

    add comment

--- golang_TourGolang/04_methods_and_interfaces/11_interface-values/main.go
+++ golang_TourGolang/04_methods_and_interfaces/11_interface-values/main.go
@@ -5,20 +5,25 @@ import (
 	"math"
 )
 
+// I comment
 type I interface {
 	M()
 }
 
+// T comment
 type T struct {
 	S string
 }
 
+// T.M comment
 func (t *T) M() {
 	fmt.Println(t.S)
 }
 
+// F comment
 type F float64
 
+// F.M comment
 func (f F) M() {
 	fmt.Println(f)
 }
@@ -38,4 +43,3 @@ func main() {
 func describe(i I) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
-
