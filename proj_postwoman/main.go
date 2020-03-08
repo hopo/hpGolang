46d3378e14e4b1ca51ea34682dd53dfda187a06c
hopo <hpsight@gmail.com>
2020-03-08 15:10:02 +0900

    resp fmprintln

--- proj_postwoman/main.go
+++ proj_postwoman/main.go
@@ -8,14 +8,28 @@ import (
 )
 
 func main() {
-	fmt.Println("= = = postwoman run server = = =")
+	log.Println(" = = = = = postwoman run server = = = = =")
 	router := mux.NewRouter()
+	router.HandleFunc("/", GetIndexEndpoint).Methods("GET")
 	router.HandleFunc("/api", GetAPIEndpoint).Methods("GET")
+
 	http.ListenAndServe(":8080", router)
+}
+
+func GetIndexEndpoint(resp http.ResponseWriter, req *http.Request) {
+	message := "CALLED~ GetIndexEndpoint"
+	log.Println(message)
+
+	resp.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(resp, "<script>alert(\"move /api\");window.location.pathname=\"/api\"</script>")
 }
 
 func GetAPIEndpoint(resp http.ResponseWriter, req *http.Request) {
 	message := "CALLED~ GetAPIEndpoint"
 	log.Println(message)
-	resp.Write([]byte(`{"message": "` + message + `"}`))
+
+	resp.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//resp.Write([]byte(`{"message": "` + message + `"}`))
+
+	fmt.Fprintln(resp, "<h1>"+message+"</h1>")
 }
