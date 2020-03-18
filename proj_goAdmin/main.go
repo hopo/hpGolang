e1e67c82f216c91ba12202cb25824fb3c21f65dd
hopo <hpsight@gmail.com>
2020-03-18 19:36:25 +0900

    .

--- proj_goAdmin/main.go
+++ proj_goAdmin/main.go
@@ -6,22 +6,18 @@ import (
 	_ "github.com/GoAdminGroup/go-admin/adapter/gin"
 	"github.com/GoAdminGroup/go-admin/engine"
 
+	"proj_goadmin/tables"
+
 	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/sqlite" // sql driver
 	"github.com/GoAdminGroup/go-admin/plugins/admin"
 	"github.com/GoAdminGroup/go-admin/plugins/example"
 	_ "github.com/GoAdminGroup/themes/adminlte"
 	"github.com/gin-gonic/gin"
-	"proj_goadmin/tables"
 	//"github.com/GoAdminGroup/go-admin/modules/config"
 	//_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql"
 )
 
-func main() {
-	r := gin.Default()
-
-	//gin.SetMode(gin.ReleaseMode)
-	gin.SetMode(gin.DebugMode)
-
+func goadminInit(ctx *gin.Engine) {
 	eng := engine.Default()
 
 	adminPlugin := admin.NewAdmin(tables.Generators)
@@ -31,18 +27,32 @@ func main() {
 
 	examplePlugin := example.NewExample()
 
-	err := eng.AddConfigFromJSON("./config.json").AddPlugins(adminPlugin, examplePlugin).Use(r)
+	err := eng.AddConfigFromJSON("./config.json").
+		AddPlugins(adminPlugin, examplePlugin).
+		Use(ctx)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	r := gin.Default()
+
+	//gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.DebugMode)
+
+	goadminInit(r)
 
 	r.Static("/uploads", "./uploads")
 
-	r.GET("/admin", func(ctx *gin.Context) {
-		fmt.Println("### CALLED~~ \"/admin\"")
-		return
-	})
+	r.GET("/admin", EndpointGetAdmin)
 
 	fmt.Println("= = = = = Run Server = = = = =")
 	r.Run(":9090")
+}
+
+// EndpointGetAdmin "/admin"
+func EndpointGetAdmin(ctx *gin.Context) {
+	fmt.Println("### CALLED~~ \"/admin\"")
+	return
 }
