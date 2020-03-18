b1e04fa0b8c224c195214f2a6941659d321a3645
hopo <hpsight@gmail.com>
2020-03-18 16:50:31 +0900

    ?

---
+++ proj_goAdmin/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/GoAdminGroup/go-admin/adapter/gin"
+	"github.com/GoAdminGroup/go-admin/engine"
+
+	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/sqlite" // sql driver
+	"github.com/GoAdminGroup/go-admin/plugins/admin"
+	"github.com/GoAdminGroup/go-admin/plugins/example"
+	_ "github.com/GoAdminGroup/themes/adminlte"
+	"github.com/gin-gonic/gin"
+	//"proj_goadmin/tables"
+	//"github.com/GoAdminGroup/go-admin/modules/config"
+	//_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql"
+)
+
+func main() {
+	r := gin.Default()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	eng := engine.Default()
+
+	adminPlugin := admin.NewAdmin()
+	//adminPlugin := admin.NewAdmin(tables.Generators)
+	//adminPlugin.AddGenerator("users", tables.GetUsersTable)
+
+	examplePlugin := example.NewExample()
+
+	err := eng.AddConfigFromJSON("./config.json").AddPlugins(adminPlugin, examplePlugin).Use(r)
+	if err != nil {
+		panic(err)
+	}
+
+	r.Static("/uploads", "./uploads")
+
+	r.GET("/admin", func(ctx *gin.Context) {
+		fmt.Println("CALLED~~ \"/admin\"")
+		return
+	})
+
+	fmt.Println("= = = = = Run Server = = = = =")
+	r.Run(":9090")
+}
